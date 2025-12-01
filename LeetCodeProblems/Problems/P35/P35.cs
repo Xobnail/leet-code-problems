d4df2d87844224771b031779cf0225ad65d000ff
Nail Khabibullin <xobnail@gmail.com>
2025-12-01 15:25:15 +0300

    P35

---
+++ LeetCodeProblems/Problems/P35/P35.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCodeProblems.Problems.P35;
+
+/// <summary>
+/// 35. Search Insert Position
+/// </summary>
+internal class P35
+{
+    public int SearchInsert(int[] nums, int target)
+    {
+        int l = 0;
+        int r = nums.Length - 1;
+        int m = 0;
+
+        while (l <= r)
+        {
+            m = (l + r) / 2;
+
+            if (nums[m] == target)
+            {
+                return m;
+            }
+
+            if (target < nums[m])
+            {
+                r = m - 1;
+            }
+            else
+            {
+                l = m + 1;
+            }
+        }
+
+        return nums[m] > target ? m : m + 1;
+    }
+}
