2c90bda5a9daf141e66fc66e3c7a6c51e36d6c09
Nail Khabibullin <xobnail@gmail.com>
2025-12-06 12:05:48 +0300

    P2331

---
+++ LeetCodeProblems/Problems/P2331/P2331.cs
@@ -0,0 +1,20 @@
+﻿using LeetCodeProblems.Models;
+
+namespace LeetCodeProblems.Problems.P2331;
+
+/// <summary>
+/// 2331. Evaluate Boolean Binary Tree
+/// </summary>
+internal class P2331
+{
+    public bool EvaluateTree(TreeNode root)
+    {
+        return root.val switch
+        {
+            1 => true,
+            2 => EvaluateTree(root.left) || EvaluateTree(root.right),
+            3 => EvaluateTree(root.left) && EvaluateTree(root.right),
+            _ => false,
+        };
+    }
+}
