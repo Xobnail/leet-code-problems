b3020c27378bf700315c12b1c9afc62b09a3c76f
Nail Khabibullin <xobnail@gmail.com>
2025-12-07 14:29:37 +0300

    P2236

---
+++ LeetCodeProblems/Problems/P2236/P2236.cs
@@ -0,0 +1,14 @@
+﻿using LeetCodeProblems.Models;
+
+namespace LeetCodeProblems.Problems.P2236;
+
+/// <summary>
+/// 2236. Root Equals Sum of Children
+/// </summary>
+internal class P2236
+{
+    public bool CheckTree(TreeNode root)
+    {
+        return root.val == root.left.val + root.right.val;
+    }
+}
