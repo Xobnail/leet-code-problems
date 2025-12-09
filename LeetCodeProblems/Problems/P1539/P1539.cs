2f827ffee4d8a27748f3ef64abba441be3955045
Nail Khabibullin <xobnail@gmail.com>
2025-12-09 14:33:49 +0300

    P1539

---
+++ LeetCodeProblems/Problems/P1539/P1539.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCodeProblems.Problems.P1539;
+
+/// <summary>
+/// 1539. Kth Missing Positive Number
+/// </summary>
+internal class P1539
+{
+    public int FindKthPositive(int[] arr, int k)
+    {
+        int l = 0;
+        int r = arr.Length;
+
+        while (l < r)
+        {
+            int m = (l + r) / 2;
+            int val = arr[m];
+
+            if (val - (m + 1) >= k)
+            {
+                r = m;
+            }
+            else
+            {
+                l = m + 1;
+            }
+        }
+
+        return l + k;
+    }
+}
