7ed18653c0b35f54b04f3fc778e8675e495ff3af
Nail Khabibullin <xobnail@gmail.com>
2025-12-11 13:10:03 +0300

    P1290

---
+++ LeetCodeProblems/Problems/1290. Convert Binary Number in a Linked List to Integer/P1290.cs
@@ -0,0 +1,70 @@
+﻿using LeetCodeProblems.Models;
+using System.Text;
+
+namespace LeetCodeProblems.Problems._1290._Convert_Binary_Number_in_a_Linked_List_to_Integer;
+
+/// <summary>
+/// 1290. Convert Binary Number in a Linked List to Integer
+/// </summary>
+internal class P1290
+{
+    public int GetDecimalValue(ListNode head)
+    {
+        int result = 0;
+
+        while (head != null)
+        {
+            result = result << 1 | head.val;
+            head = head.next;
+        }
+
+        return result;
+    }
+
+    // По методу Горнера
+    //public int GetDecimalValue(ListNode head)
+    //{
+    //    int result = 0;
+
+    //    while (head != null)
+    //    {
+    //        result = result * 2 + head.val;
+    //        head = head.next;
+    //    }
+
+    //    return result;
+    //}
+
+    //public int GetDecimalValue(ListNode head)
+    //{
+    //    Stack<int> values = new();
+
+    //    while (head != null)
+    //    {
+    //        values.Push(head.val);
+    //        head = head.next;
+    //    }
+
+    //    int result = 0;
+
+    //    for (int i = 0; values.TryPop(out int value); i++)
+    //    {
+    //        result += value * (int)Math.Pow(2, i);
+    //    }
+
+    //    return result;
+    //}
+
+    //public int GetDecimalValue(ListNode head)
+    //{
+    //    StringBuilder sb = new();
+
+    //    while (head != null)
+    //    {
+    //        sb.Append(head.val);
+    //        head = head.next;
+    //    }
+
+    //    return Convert.ToInt32(sb.ToString(), 2);
+    //}
+}
