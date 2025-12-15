47db13fb7f8bb64e3c6dadfb5ddd9e7e2fc57001
Nail Khabibullin <xobnail@gmail.com>
2025-12-15 12:29:29 +0300

    P1351+

--- LeetCodeProblems/Problems/1351. Count Negative Numbers in a Sorted Matrix/P1351.cs
+++ LeetCodeProblems/Problems/1351. Count Negative Numbers in a Sorted Matrix/P1351.cs
@@ -11,41 +11,62 @@ internal class P1351
 
         int result = 0;
 
-        for (int i = 0; i < grid.Length; i++)
+        for (int i = grid.Length - 1, j = 0; i >= 0 && j < grid[0].Length;)
         {
-            result += CountNegatives(grid[i]);
+            if (grid[i][j] < 0)
+            {
+                result += grid[i--].Length - j;
+            }
+            else
+            {
+                j++;
+            }
         }
 
         return result;
     }
 
-    public int CountNegatives(int[] row)
-    {
-        if (row.Length == 1)
-        {
-            return row[0] < 0 ? 1 : 0;
-        }
+    //public int CountNegatives(int[][] grid)
+    //{
+    //    if (grid.Length == 0) return 0;
 
-        int l = 0;
-        int r = row.Length - 1;
+    //    int result = 0;
 
-        while (l < r)
-        {
-            int m = (l + r) / 2;
+    //    for (int i = 0; i < grid.Length; i++)
+    //    {
+    //        result += CountNegatives(grid[i]);
+    //    }
 
-            if (row[m] >= 0)
-            {
-                l = m + 1;
-            }
+    //    return result;
+    //}
 
-            if (row[m] < 0)
-            {
-                r = m;
-            }
-        }        
+    //public int CountNegatives(int[] row)
+    //{
+    //    if (row.Length == 1)
+    //    {
+    //        return row[0] < 0 ? 1 : 0;
+    //    }
 
-        return row[r] < 0 ? row.Length - r : 0;
-    }
+    //    int l = 0;
+    //    int r = row.Length - 1;
+
+    //    while (l < r)
+    //    {
+    //        int m = (l + r) / 2;
+
+    //        if (row[m] >= 0)
+    //        {
+    //            l = m + 1;
+    //        }
+
+    //        if (row[m] < 0)
+    //        {
+    //            r = m;
+    //        }
+    //    }        
+
+    //    return row[r] < 0 ? row.Length - r : 0;
+    //}
 
     //public int CountNegatives(int[][] grid)
     //{
