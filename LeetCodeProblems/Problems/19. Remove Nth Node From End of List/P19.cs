8486b514df39f5dba9f37558c54b3cdefcb973bd
Nail Khabibullin <xobnail@gmail.com>
2025-12-21 12:38:58 +0300

    P19

---
+++ LeetCodeProblems/Problems/19. Remove Nth Node From End of List/P19.cs
@@ -0,0 +1,35 @@
+﻿using LeetCodeProblems.Models;
+
+namespace LeetCodeProblems.Problems._19._Remove_Nth_Node_From_End_of_List;
+
+/// <summary>
+/// 19. Remove Nth Node From End of List
+/// </summary>
+internal class P19
+{
+    public ListNode RemoveNthFromEnd(ListNode head, int n)
+    {
+        if (head.next == null) return null;
+        if (head.next.next == null && n == 1) return new ListNode(head.val);
+
+        ListNode fast = head;
+        ListNode dummy = head;
+
+        for (int i = 0; i < n; i++)
+        {
+            fast = fast?.next;
+        }
+
+        if (fast == null) return head.next;
+
+        while (fast.next != null)
+        {
+            head = head.next;
+            fast = fast.next;
+        }
+
+        head.next = head.next.next;
+
+        return dummy;
+    }
+}
