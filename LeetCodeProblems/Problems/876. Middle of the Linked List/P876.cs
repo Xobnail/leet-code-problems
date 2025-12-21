ca99a0d46f8fc1b8c19c0a9d6f08420bf231c91f
Nail Khabibullin <xobnail@gmail.com>
2025-12-21 11:01:31 +0300

    P876

---
+++ LeetCodeProblems/Problems/876. Middle of the Linked List/P876.cs
@@ -0,0 +1,36 @@
+﻿using LeetCodeProblems.Models;
+
+namespace LeetCodeProblems.Problems._876._Middle_of_the_Linked_List;
+
+/// <summary>
+/// 876. Middle of the Linked List
+/// </summary>
+internal class P876
+{
+    public ListNode MiddleNode(ListNode head)
+    {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast?.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    //public ListNode MiddleNode(ListNode head)
+    //{
+    //    List<ListNode> list = new();
+
+    //    while (head != null)
+    //    {
+    //        list.Add(head);
+    //        head = head.next;
+    //    }
+
+    //    return list[list.Count / 2];
+    //}
+}
