cc3ac026d475de6dcfa60cae814295f3f3a2d7d7
Nail Khabibullin <xobnail@gmail.com>
2025-12-22 11:29:34 +0300

    P700

---
+++ LeetCodeProblems/Problems/700. Search in a Binary Search Tree/P700.cs
@@ -0,0 +1,21 @@
+﻿using LeetCodeProblems.Models;
+
+namespace LeetCodeProblems.Problems._700._Search_in_a_Binary_Search_Tree;
+
+/// <summary>
+/// 700. Search in a Binary Search Tree
+/// </summary>
+internal class P700
+{
+    public TreeNode SearchBST(TreeNode root, int val)
+    {
+        if (root == null) 
+            return null;
+        else if(root.val > val) 
+            return SearchBST(root.left, val);
+        else if (root.val < val) 
+            return SearchBST(root.right, val);
+        else
+            return root;
+    }
+}
