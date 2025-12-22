e51e01b67cd624483b5f3066d09a921653b4f16c
Nail Khabibullin <xobnail@gmail.com>
2025-12-22 11:43:12 +0300

    P701

---
+++ LeetCodeProblems/Problems/701. Insert into a Binary Search Tree/P701.cs
@@ -0,0 +1,36 @@
+﻿using LeetCodeProblems.Models;
+
+namespace LeetCodeProblems.Problems._701._Insert_into_a_Binary_Search_Tree;
+
+/// <summary>
+/// 701. Insert into a Binary Search Tree
+/// </summary>
+internal class P701
+{
+    public TreeNode InsertIntoBST(TreeNode root, int val)
+    {
+        if (root == null) return new TreeNode(val);
+
+        if (root.val > val && root.left == null)
+        {
+            root.left = new TreeNode(val);
+            return root;
+        }
+        if (root.val < val && root.right == null)
+        {
+            root.right = new TreeNode(val);
+            return root;
+        }
+
+        if (root.val > val)
+        {
+            InsertIntoBST(root.left, val);
+        }
+        else
+        {
+            InsertIntoBST(root.right, val);
+        }
+
+        return root;
+    }
+}
