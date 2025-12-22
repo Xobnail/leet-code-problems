2ac7a21850a1efa0ec3c6bf0ed721c70f852c580
Nail Khabibullin <xobnail@gmail.com>
2025-12-22 14:18:57 +0300

    P998

---
+++ LeetCodeProblems/Problems/998. Maximum Binary Tree II/P998.cs
@@ -0,0 +1,20 @@
+﻿using LeetCodeProblems.Models;
+
+namespace LeetCodeProblems.Problems._998._Maximum_Binary_Tree_II;
+
+/// <summary>
+/// 998. Maximum Binary Tree II
+/// </summary>
+internal class P998
+{
+    public TreeNode InsertIntoMaxTree(TreeNode root, int val)
+    {
+        if (root == null) return new TreeNode(val);
+
+        if (root.val < val) return new TreeNode(val, root);
+
+        root.right = InsertIntoMaxTree(root.right, val);
+
+        return root;
+    }
+}
