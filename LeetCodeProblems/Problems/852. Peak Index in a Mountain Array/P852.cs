6fe2f170584451ae671f7eaf0d500124d861ee24
Nail Khabibullin <xobnail@gmail.com>
2025-12-24 17:28:04 +0300

    P852

---
+++ LeetCodeProblems/Problems/852. Peak Index in a Mountain Array/P852.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCodeProblems.Problems._852._Peak_Index_in_a_Mountain_Array;
+
+/// <summary>
+/// 852. Peak Index in a Mountain Array
+/// </summary>
+internal class P852
+{
+    public int PeakIndexInMountainArray(int[] arr)
+    {
+        int l = 1;
+        int r = arr.Length - 1;
+
+        while (l < r)
+        {
+            int m = (l + r) / 2;
+
+            if (arr[m - 1] < arr[m])
+            {
+                l = m + 1;
+            }
+            else
+            {
+                r = m;
+            }
+        }
+
+        return l - 1;
+    }
+}
