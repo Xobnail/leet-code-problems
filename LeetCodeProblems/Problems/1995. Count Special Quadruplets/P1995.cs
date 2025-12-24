cb7fd0824aa5fc7bb5767c2e10d1beab3c0c5e80
Nail Khabibullin <xobnail@gmail.com>
2025-12-24 14:01:58 +0300

    P1995

---
+++ LeetCodeProblems/Problems/1995. Count Special Quadruplets/P1995.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCodeProblems.Problems._1995._Count_Special_Quadruplets;
+
+internal class P1995
+{
+    public int CountQuadruplets(int[] nums)
+    {
+        int result = 0;
+
+        for (int a = 0; a < nums.Length; a++)
+            for (int b = a + 1; b < nums.Length; b++)
+                for (int c = b + 1; c < nums.Length; c++)
+                    for (int d = c + 1; d < nums.Length; d++)
+                        if (nums[a] + nums[b] + nums[c] == nums[d])
+                            result++;
+
+        return result;
+    }
+}
