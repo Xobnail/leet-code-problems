1b87d71866c623e9fbc8318b114f34ac0819a033
Nail Khabibullin <xobnail@gmail.com>
2025-12-25 12:05:19 +0300

    P637

---
+++ LeetCodeProblems/Problems/637. Average of Levels in Binary Tree/P637.cs
@@ -0,0 +1,41 @@
+﻿using LeetCodeProblems.Models;
+
+namespace LeetCodeProblems.Problems._637._Average_of_Levels_in_Binary_Tree;
+
+/// <summary>
+/// 637. Average of Levels in Binary Tree
+/// </summary>
+internal class P637
+{
+    public IList<double> AverageOfLevels(TreeNode root)
+    {
+        Queue<TreeNode> q = [];
+        q.Enqueue(root);
+        IList<double> result = [];
+
+        while (q.Count > 0)
+        {
+            int levelLength = q.Count;
+            double sum = 0;
+
+            for (int i = 0; i < levelLength; i++)
+            {
+                TreeNode node = q.Dequeue();
+                sum += node.val;
+
+                if (node.left != null)
+                {
+                    q.Enqueue(node.left);
+                }
+                if (node.right != null)
+                {
+                    q.Enqueue(node.right);
+                }
+            }
+
+            result.Add(sum / levelLength);
+        }
+
+        return result;
+    }
+}
