b9b3d00dc3536901b5b18554b173f9e73ab35b1e
Nail Khabibullin <xobnail@gmail.com>
2025-12-27 11:39:47 +0300

    P221

---
+++ LeetCodeProblems/Problems/221. Maximal Square/P221.cs
@@ -0,0 +1,44 @@
+﻿namespace LeetCodeProblems.Problems._221._Maximal_Square;
+
+/// <summary>
+/// 221. Maximal Square
+/// </summary>
+internal class P221
+{
+    public int MaximalSquare(char[][] matrix)
+    {
+        int m = matrix.Length;
+        int n = matrix[0].Length;
+        int[,] mem = new int[m + 1, n + 1];
+        int maxSquareSide = 0;
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (matrix[i][j] != '1')
+                {
+                    continue;
+                }
+
+                int topLeft = mem[i, j];
+                int topRight = mem[i, j + 1];
+                int bottomLeft = mem[i + 1, j];
+
+                mem[i + 1, j + 1] = Min(topLeft, bottomLeft, topRight) + 1;
+
+                if (mem[i + 1, j + 1] > maxSquareSide)
+                {
+                    maxSquareSide = mem[i + 1, j + 1];
+                }
+            }
+        }
+
+        return maxSquareSide * maxSquareSide;
+    }
+
+    public int Min(int a, int b, int c)
+    {
+        return Math.Min(a, Math.Min(b, c));
+    }
+}
