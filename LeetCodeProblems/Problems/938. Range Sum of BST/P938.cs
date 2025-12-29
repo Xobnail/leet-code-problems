e1c2e7b40b80e18435a37bcfa67f1f223ccb2d25
Nail Khabibullin <xobnail@gmail.com>
2025-12-29 12:07:50 +0300

    P938

---
+++ LeetCodeProblems/Problems/938. Range Sum of BST/P938.cs
@@ -0,0 +1,34 @@
+﻿using LeetCodeProblems.Models;
+
+namespace LeetCodeProblems.Problems._938._Range_Sum_of_BST;
+
+/// <summary>
+/// 938. Range Sum of BST
+/// </summary>
+internal class P938
+{
+    public int RangeSumBST(TreeNode root, int low, int high)
+    {
+        if (root == null)
+        {
+            return 0;
+        }
+
+        int result = 0;
+
+        if (low <= root.val && root.val <= high)
+        {
+            result += root.val;
+            result += RangeSumBST(root.right, low, high);
+            result += RangeSumBST(root.left, low, high);
+        }
+        else
+        {
+            result += root.val < low
+                ? RangeSumBST(root.right, low, high)
+                : RangeSumBST(root.left, low, high);
+        }
+
+        return result;
+    }
+}
