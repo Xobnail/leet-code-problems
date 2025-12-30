043dbb9b5d4355b534111285d1a1ef5f89406486
Nail Khabibullin <xobnail@gmail.com>
2025-12-30 15:13:04 +0300

    P2133

---
+++ LeetCodeProblems/Problems/2133. Check if Every Row and Column Contains All Numbers/P2133.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCodeProblems.Problems._2133._Check_if_Every_Row_and_Column_Contains_All_Numbers;
+
+/// <summary>
+/// 2133. Check if Every Row and Column Contains All Numbers
+/// </summary>
+internal class P2133
+{
+    public bool CheckValid(int[][] matrix)
+    {
+        for (int i = 0; i < matrix.Length; i++)
+        {
+            HashSet<int> row = [];
+            HashSet<int> col = [];
+
+            for (int j = 0; j < matrix.Length; j++)
+            {
+                row.Add(matrix[i][j]);
+                col.Add(matrix[j][i]);
+            }
+
+            for (int j = 1; j <= matrix.Length; j++)
+            {
+                if (!row.Contains(j) || !col.Contains(j))
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
