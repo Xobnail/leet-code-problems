f6dc5c0aa64d1b96c5b74f5b54370ab1b1c2dee5
Nail Khabibullin <xobnail@gmail.com>
2025-12-30 13:08:51 +0300

    P268

---
+++ LeetCodeProblems/Problems/268. Missing Number/P268.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCodeProblems.Problems._268._Missing_Number;
+
+/// <summary>
+/// 268. Missing Number
+/// </summary>
+internal class P268
+{
+    public int MissingNumber(int[] nums)
+    {
+        int sum = 0;
+
+        for (int i = 0; i <= nums.Length; i++)
+            sum += i;
+
+        for (int i = 0; i < nums.Length; i++)
+            sum -= nums[i];
+
+        return sum;
+    }
+
+    //public int MissingNumber(int[] nums)
+    //{
+    //    HashSet<int> mem = new(nums);
+
+    //    for (int i = 0; i <= nums.Length; i++)
+    //        if (!mem.Contains(i))
+    //            return i;
+
+    //    throw new Exception();
+    //}
+}
