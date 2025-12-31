40152d71303b5f534da95d0ca6b8e11a0dc3ef99
Nail Khabibullin <xobnail@gmail.com>
2025-12-31 11:46:15 +0300

    P116

---
+++ LeetCodeProblems/Problems/116. Populating Next Right Pointers in Each Node/P116.cs
@@ -0,0 +1,46 @@
+﻿namespace LeetCodeProblems.Problems._116._Populating_Next_Right_Pointers_in_Each_Node;
+
+/// <summary>
+/// 116. Populating Next Right Pointers in Each Node
+/// </summary>
+internal class P116
+{
+    public Node Connect(Node root)
+    {
+        if (root?.left == null)
+        {
+            return root;
+        }
+
+        root.left.next = root.right;
+        root.right.next = root.next?.left;
+
+        Connect(root.left);
+        Connect(root.right);
+
+        return root;
+    }
+}
+
+public class Node
+{
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() { }
+
+    public Node(int _val)
+    {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next)
+    {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+}
